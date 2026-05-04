20e5f0ef61fcb4a11fd22bbe55f5555eab9cba73
khoa21112004-web <khoa21112004@gmail.com>
2026-05-04 21:59:35 +0700

    Update model.py

--- DeepLearning-main/src/model.py
+++ DeepLearning-main/src/model.py
@@ -7,21 +7,20 @@ class TripleMRNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        m = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-        self.encoder = nn.Sequential(*list(m.children())[:-1])
+        base = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+        self.encoder = nn.Sequential(*list(base.children())[:-1])
 
         self.fc = nn.Linear(512 * 3, 1)
 
     def encode(self, vol):
         vol = vol.squeeze(0)
-        f = self.encoder(vol)
-        f = f.view(f.size(0), -1)
-        return f.mean(dim=0, keepdim=True)  # 🔥 mean pooling
+        feat = self.encoder(vol)
+        feat = feat.view(feat.size(0), -1)
+        return feat.mean(dim=0, keepdim=True)
 
     def forward(self, ax, sa, co):
         x = self.encode(ax)
         y = self.encode(sa)
         z = self.encode(co)
 
-        w = torch.cat([x, y, z], dim=1)
-        return self.fc(w)
+        return self.fc(torch.cat([x, y, z], dim=1))
