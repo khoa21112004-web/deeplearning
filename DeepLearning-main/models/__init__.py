6e39f43e3c62354f04d9a6cefaa15c557d71e4ff
khoa21112004 <khoa21112004@gmail.com>
2026-05-28 16:06:21 +0700

    Replace old code with optimized EfficientNet training pipeline

--- DeepLearning-main/models/__init__.py
+++
@@ -1,7 +0,0 @@
-try:
-    from .MRnet import MRnet
-except Exception:
-    MRnet = None
-from .Densenet121 import Densenet121
-from .EfficientNetB0 import EfficientNetB0
-from .EfficientNetB0_SE_ViT import EfficientNetB0_SE_ViT
